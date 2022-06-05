bc037788e4cb042e742e30d8308fc0a25f054dea
diayan <diayans@outlook.com>
2022-06-05 01:34:08 +0000

    add content view as the main view of the app

---
+++ PetScout/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PetScout
+//
+//  Created by diayan siat on 04/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
