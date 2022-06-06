fc63cba6e9edaf982e777bfcec29fca86d08c6f6
diayan <diayans@outlook.com>
2022-06-06 22:08:46 +0000

    create access token manager to be used later to store access token && create user default keys

---
+++ PetScout/Core/utils/AppUserDefaultsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  AppUserDefaultsKeys.swift
+//  PetScout
+//
+//  Created by diayan siat on 06/06/2022.
+//
+
+import Foundation
+
+enum AppUserDefaultsKeys {
+  static let expiresAt = "expiresAt"
+  static let bearerAccessToken = "bearerAccessToken"
+}
