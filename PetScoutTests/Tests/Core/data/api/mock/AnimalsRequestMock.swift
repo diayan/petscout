e2ba0afa1289ed38b8eb9fde90dca5be872a70f6
diayan <diayans@outlook.com>
2022-06-07 21:58:21 +0000

    create animal request mock to fetch mock data in from preview contents

---
+++ PetScoutTests/Tests/Core/data/api/mock/AnimalsRequestMock.swift
@@ -0,0 +1,26 @@
+//
+//  AnimalRequestMock.swift
+//  PetScoutTests
+//
+//  Created by diayan siat on 07/06/2022.
+//
+
+import Foundation
+@testable import PetScout
+
+enum AnimalsRequestMock: RequestProtocol {
+    case getAnimals
+    
+    var path: String {
+        guard let path = Bundle.main.path(
+            forResource: "AnimalsMock", ofType: "json"
+        )
+        else { return "" }
+        
+        return path
+    }
+    
+    var requestType: RequestType {
+        .GET
+    }
+}
