ee03d2e568e8dc658b709cdec6c911ea63788c8f
diayan <diayans@outlook.com>
2022-06-07 08:06:34 +0000

    fetch animals near you in  using the network manager in AnimalsNearYouView && create a variable picture

--- PetScout/AnimalsNearYou/views/AnimalsNearYouView.swift
+++ PetScout/AnimalsNearYou/views/AnimalsNearYouView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct AnimalsNearYouView: View {
+    @State var animals: [Animal] = []
+    @State var isLoading = true
+    private let requestManager = RequestManager()
+
   var body: some View {
-    NavigationView {
-      Text("TODO: Animals Near You View")
+      NavigationView {
+        List {
+          ForEach(animals) { animal in
+            AnimalRow(animal: animal)
+          }
+        }
+        .task {
+          await fetchAnimals()
+        }
+        .listStyle(.plain)
         .navigationTitle("Animals near you")
-    }.navigationViewStyle(StackNavigationViewStyle())
+        .overlay {
+          if isLoading {
+            ProgressView("Finding Animals near you...")
+          }
+        }
+      }.navigationViewStyle(StackNavigationViewStyle())
+
   }
+    
+    func fetchAnimals() async {
+      do {
+        let animalsContainer: AnimalsContainer =
+          try await requestManager.perform(AnimalsRequest.getAnimalsWith(
+            page: 1,
+            latitude: nil,
+            longitude: nil))
+        self.animals = animalsContainer.animals
+        await stopLoading()
+      } catch {}
+    }
+
+    @MainActor
+    func stopLoading() async {
+      isLoading = false
+    }
 }
 
 struct AnimalsNearYouView_Previews: PreviewProvider {
