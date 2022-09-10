3cfaa2c694caf29264854767783c85828211ed1c
diayan <diayans@outlook.com>
2022-09-10 13:56:33 +0000

    write unit test to check if data is being stored in core data

--- PetScoutTests/Tests/Core/data/coreData/CoreDataTests.swift
+++ PetScoutTests/Tests/Core/data/coreData/CoreDataTests.swift
@@ -11,10 +11,38 @@ import CoreData
 
 class CoreDataTests: XCTestCase {
     override func setUpWithError() throws {
-      try super.setUpWithError()
+        try super.setUpWithError()
     }
-
+    
     override func tearDownWithError() throws {
-      try super.tearDownWithError()
+        try super.tearDownWithError()
+    }
+    
+    func testToManagedObject() throws {
+        let previewContext =  PersistenceController.shared.container.viewContext
+        
+        let fetchRequest = AnimalEntity.fetchRequest()
+        fetchRequest.fetchLimit = 1
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \AnimalEntity.name, ascending: true)]
+        guard let results = try? previewContext.fetch(fetchRequest),
+        let first = results.first else { return }
+        
+        XCTAssert(first.name == "CHARLA", """
+    Pet name did not match, was expecting Kiki, got
+    \(String(describing: first.name))
+"""
+        )
+        
+        XCTAssert(first.type == "Dog", """
+                  Pet type did not match, was expecting Cat, got
+                  \(String(describing: first.type))
+                """
+        )
+        
+        XCTAssert(first.coat.rawValue == "Short", """
+     Pet type did not match, was expecting Cat, got
+    \(String(describing: first.coat.rawValue))
+  """
+        )
     }
 }
