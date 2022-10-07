8a8b047b095825bd2ec8f5600c67b7f4f83511d1
diayan <diayans@outlook.com>
2022-10-07 13:32:36 +0000

    create a viewmodel to handle the animal near you view

---
+++ PetScout/AnimalsNearYou/viewModels/AnimalsNearYouViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsNearYouViewModel.swift
+//  PetScout
+//
+//  Created by diayan siat on 07/10/2022.
+//
+
+import Foundation
+
+//for AnimalsNearYouView to store states and handle request and user interactions
+//final classes cannot be overriden because they disallow subclassing
+//final is also a compiler optimzer for speeding up build times
+//the ObserverableObject protocol allows you to observe & make changes to the UI
+//from outside the view
+final class AnimalsNearYouViewModel: ObservableObject {
+    
+}
