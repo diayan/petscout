6c3582fe0193f1a89d8298648e7bbcb1e3132dd8
diayan <diayans@outlook.com>
2022-10-09 09:40:07 +0000

    write a test case for when there is an empty response

--- PetScoutTests/Tests/AnimalsNearYou/AnimalsNearYouViewModelTestsCase.swift
+++ PetScoutTests/Tests/AnimalsNearYou/AnimalsNearYouViewModelTestsCase.swift
@@ -29,10 +29,31 @@ final class AnimalsNearYouViewModelTestCase: XCTestCase {
         XCTAssertFalse(viewModel.isLoading, "view shouldn't be loading but it is loading")
     }
     
-    //tests if the page is 1 before scroll and checks again after more data has been fetched 
+    //tests if the page is 1 before scroll and checks again after more data has been fetched
     func testUpdatePageOnFetchMoreAnimals() async {
         XCTAssertEqual(viewModel.page, 1, "the view model's page property should be 1 before fetching but it's \(viewModel.page)")
         await viewModel.fetchMoreAnimals()
         XCTAssertEqual(viewModel.page, 2, "the view model's page property should be 2 after fetching, but it's \(viewModel.page)")
+    }
+    
+    func testFetchAnimalsEmptyResponseTestCase() async {
+        viewModel = AnimalsNearYouViewModel(
+            isLoading: true,
+            animalFetcher: EmptyResponseAnimalsFetcherMock(),
+            animalStore: AnimalStoreService(context: testContext)
+        )
+        
+        await viewModel.fetchAnimals()
+        
+        XCTAssertFalse(viewModel.hasMoreAnimals, "hasMoreAnimals should be false with an empty response but it's true")
+        XCTAssertFalse(viewModel.isLoading, "the view model shouldn't be loading after receiving an empty response, but it is")
+    }
+}
+
+
+//a struct to mock empty response from the API
+struct EmptyResponseAnimalsFetcherMock: AnimalsFetcher {
+    func fetchAnimals(page: Int) async -> [Animal] {
+        return []
     }
 }
