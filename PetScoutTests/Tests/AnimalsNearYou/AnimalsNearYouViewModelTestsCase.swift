eaa994bc1c19e261f7c7de84a61969828b22c315
diayan <diayans@outlook.com>
2022-10-09 09:19:37 +0000

    test isLoading in viewmodel

--- PetScoutTests/Tests/AnimalsNearYou/AnimalsNearYouViewModelTestsCase.swift
+++ PetScoutTests/Tests/AnimalsNearYou/AnimalsNearYouViewModelTestsCase.swift
@@ -5,22 +5,27 @@
 //  Created by diayan siat on 08/10/2022.
 //
 
+import Foundation
 import XCTest
 @testable import PetScout
 
 @MainActor
 final class AnimalsNearYouViewModelTestCase: XCTestCase {
     let testContext = PersistenceController.preview.container.viewContext
-    
-    var viewModel: AnimalsNearYouViewModel
+    var viewModel: AnimalsNearYouViewModel!
     
     @MainActor
     override func setUp() {
         super.setUp()
-        
         viewModel = AnimalsNearYouViewModel(
             isLoading: true,
             animalFetcher: AnimalsFetcherMock(),
             animalStore: AnimalStoreService(context: testContext))
+    }
+    
+    func testFetchAnimalsLoadingState() async {
+        XCTAssertTrue(viewModel.isLoading, "view should be loading but it's not")
+        await viewModel.fetchAnimals()
+        XCTAssertFalse(viewModel.isLoading, "view shouldn't be loading but it is loading")
     }
 }
